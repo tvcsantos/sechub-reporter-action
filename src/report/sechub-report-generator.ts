3354dc5c9a9579b4540e7c84d861ed053c05445c
Tiago Santos <tvcsantos@gmail.com>
2023-08-31 15:32:41 +0100

    fix: add nobreak dash to cwe link

--- src/report/sechub-report-generator.ts
+++ src/report/sechub-report-generator.ts
@@ -13,7 +13,7 @@ const SUCCESS_COMMENT =
 const FAIL_COMMENT =
   '# :x: SecHub - We detected some findings on your code base!'
 const CWE_LINK = (id: number): string =>
-  `[CWE-${id}](https://cwe.mitre.org/data/definitions/${id}.html)`
+  `[CWE&#8209;${id}](https://cwe.mitre.org/data/definitions/${id}.html)`
 
 export class SecHubReportGenerator implements ReportGenerator {
   private context: ContextExtensions
